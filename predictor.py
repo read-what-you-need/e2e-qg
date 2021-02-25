e325c200c846263100e00fcee341f0bb165da545
soulbliss <deepak.garasangi@gmail.com>
2021-02-25 17:21:25 +0530

    remove redis coupling, custom docker

--- predictor.py
+++ predictor.py
@@ -3,10 +3,6 @@
 import os, subprocess
 import io
 
-import redis
-
-
-
 from question_generation.pipelines import pipeline
 
 class PythonPredictor:
@@ -14,40 +10,15 @@ class PythonPredictor:
     def __init__(self, config):
       self.nlp = pipeline("e2e-qg")
 
-      self.redis_host = os.getenv('REDIS_HOST')
-      self.redis_port = os.getenv('REDIS_PORT')
-      self.redis_passkey = os.getenv('REDIS_PASSKEY')
-
-      self.r = redis.StrictRedis(host=self.redis_host, port=self.redis_port, password=self.redis_passkey, ecode_responses=True)
-
-
 
         
     def predict(self, payload):
       
       text=payload['text']
       
-      if self.r.sismember('context:', text) is False:
-        print('caching to be done')
         
-        questions=self.nlp(text)
+      questions=self.nlp(text)
         
-        self.r.hset('context:'+text, text, questions[0])
-        self.r.sadd('context:', text)
+      response = questions[0]
 
-        return questions[0]
-
-        
-
-      else:
-
-        print('file available in redis cache! 😇')
-        response = self.r.hget('context:'+text, text)
-
-        return response
-      
-	
-        
-
-
-
+      return questions[0]
